eb7651644e4efc4e89539ce5c5b582960884b314
Charles Mangwa <charlesmangwa@gmail.com>
2016-06-28 14:43:46 +0200

    chore(*): initial commit

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+var path = anime.path('path');
+
+anime({
+  targets: 'rect',
+  translateX: path,
+  translateY: path,
+  rotate: path,
+  duration: 3000,
+  loop: true,
+  easing: 'linear'
+});
+
+anime({
+  targets: 'path',
+  opacity: 0,
+  duration: 6000,
+  loop: true,
+  direction: 'alternate',
+  easing: 'easeInOutExpo'
+});
+
+console.log('Yo');
