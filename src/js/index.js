c6d9e06888412a10cf95be487dcabb6291a75dcd
Lionel Paulus <hello@lionelpaul.us>
2016-06-30 22:52:42 +0200

    chore(*): video poster + new videos + JS opti

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,88 @@
 window.onload = function () {
-  var animation_part1 = false;
-  var waypoints = $('section.part1').waypoint({
-    handler: function(direction) {
-      if((animation_part1 === false)&&(direction === 'down')){
-        $('.title_part1').fadeIn(2000);
-        $('.title_partContainer').delay(3500).fadeIn(2000);
-        animation_part1 = true;
-      }
-    },
-    offset: '50%'
-  })
-  var waypoints = $('section.partContainer').waypoint({
-    handler: function(direction) {
-      if(direction === 'down'){
-        $('.partContainer-content').css('overflow-y', 'scroll');
-      }
-    }
-  })
-}
 
-function replay_video(video_id){
-  var current_video = document.getElementById(video_id);
-  current_video.pause();
-  current_video.currentTime = '0';
-  current_video.play();
+  /* User Agent */
+  var UserAgent = UserAgentAPI();
+  $('#device').append(UserAgent.data.platform_name);
+
+  var lithium = {
+    'Mac OSX El Capitan': 8,
+    'Android': 0.78,
+    'Apple iPhone': 0.84
+  };
+
+  var current_lithium = lithium[UserAgent.data.platform_name];
+
+  if (typeof current_lithium == 'undefined') {
+    current_lithium = 0.74;
+  }
+
+  $('#lithium').append(current_lithium);
+
+  // if(UserAgent.data.platform_type == 'Desktop'){
+
+    jQuery(function($) {
+      $('body').panelSnap();
+    });
+
+    var animation_part1 = false;
+    var waypoints = $('section.part1').waypoint({
+      handler: function(direction) {
+        if((animation_part1 === false)&&(direction === 'down')){
+          $('.title_part1').fadeIn(2000);
+          $('.title_partContainer').delay(2000).fadeIn(2000);
+          animation_part1 = true;
+        }
+        $('.part2-content').css('overflow-y', 'hidden');
+      },
+      offset: '50%'
+    })
+    var waypoints = $('section.part2').waypoint({
+      handler: function(direction) {
+        if(direction === 'down'){
+          $('.part2-content').css('overflow-y', 'scroll');
+          $('.part3-content').css('overflow-y', 'hidden');
+        }
+      }
+    })
+    var waypoints = $('section.part3').waypoint({
+      handler: function(direction) {
+        if(direction === 'down'){
+          $('.part2-content').css('overflow-y', 'hidden');
+          $('.part3-content').css('overflow-y', 'scroll');
+          $('.part4-content').css('overflow-y', 'hidden');
+        }
+      }
+    })
+    var waypoints = $('section.part4').waypoint({
+      handler: function(direction) {
+        if(direction === 'down'){
+          $('.part3-content').css('overflow-y', 'hidden');
+          $('.part4-content').css('overflow-y', 'scroll');
+          $('.part5-content').css('overflow-y', 'hidden');
+        }
+      }
+    })
+    var waypoints = $('section.part5').waypoint({
+      handler: function(direction) {
+        if(direction === 'down'){
+          $('.part4-content').css('overflow-y', 'hidden');
+          $('.part5-content').css('overflow-y', 'scroll');
+        }
+      }
+    })
+  // }else{
+  //   var animation_part1 = false;
+  //   var waypoints = $('section.part1').waypoint({
+  //     handler: function(direction) {
+  //       if((animation_part1 === false)&&(direction === 'down')){
+  //         $('.title_part1').fadeIn(2000);
+  //         $('.title_partContainer').delay(2000).fadeIn(2000);
+  //         animation_part1 = true;
+  //       }
+  //     },
+  //     offset: '50%'
+  //   })
+  // }
 }
 
 // Distance between two points
@@ -64,98 +123,6 @@ function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
 function deg2rad(deg) {
   return deg * (Math.PI/180)
 }
-
-// Smooth Scroll
-jQuery(function($) {
-
-  $('body').panelSnap();
-
-  var animation_part1 = false;
-  $('.part1').panelSnap({
-    onSnapFinish: function($target) {
-      if(animation_part1 === false){
-        $('.title_part1').fadeIn(2000);
-        $('.title_part2').delay(3500).fadeIn(2000);
-        animation_part1 = true;
-      }
-    }
-  });
-
-  $('.part2').panelSnap({
-    onSnapFinish: function($target) {
-      $('.part2-content').css('overflow-y', 'scroll');
-    }
-  });
-
-  // Menu demo
-  $('.menu_demo .panels').panelSnap({
-    $menu: $('.menu_demo .menu')
-  });
-
-  // Keyboard demo
-  $('.keyboard_demo .panels').panelSnap({
-    navigation: {
-      keys: {
-        nextKey: 39,
-        prevKey: 37,
-      }
-    }
-  });
-
-  // Callback demo
-  $('.callback_demo .panels').panelSnap({
-    onSnapFinish: function($target) {
-
-      log('callback', 'onSnapFinish', $target);
-
-    }
-  });
-
-  // Event demo
-  $('.event_demo .panels').on('panelsnap:start', event_log);
-  $('.event_demo .panels').on('panelsnap:finish', event_log);
-  $('.event_demo .panels').on('panelsnap:activate', event_log);
-
-  $('.event_demo .panels').panelSnap();
-
-  function event_log(e, $target) {
-
-    log('event', e.type, $target);
-
-  }
-
-  // Offset demo
-  $('.offset_demo .panels').panelSnap({
-      offset: 85
-  });
-
-  // Shared log function
-  function log(type, action, $target) {
-
-    var text = '<p>' + action + ':<br>' + $target.find('h1').text() + '</p>';
-    $('.' + type + '_demo .log h2').after(text);
-
-  }
-
-});
-
-/* User Agent */
-var UserAgent = UserAgentAPI();
-$('#device').append(UserAgent.data.platform_name);
-
-var lithium = {
-  'Mac OSX El Capitan': 8,
-  'Android': 0.78,
-  'Apple iPhone': 0.84
-};
-
-var current_lithium = lithium[UserAgent.data.platform_name];
-
-if (typeof current_lithium == 'undefined') {
-  current_lithium = 0.74;
-}
-
-$('#lithium').append(current_lithium);
 
 /* Country */
 var country = CountryAPI();
